63472ae171f83f05056e7fade05b2bb4143d8ef9
romanoz33 <57302328+romanoz33@users.noreply.github.com>
2019-11-05 21:17:36 +0300

    home_1

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,5 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-
-	// Custom JS
-
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
 });
